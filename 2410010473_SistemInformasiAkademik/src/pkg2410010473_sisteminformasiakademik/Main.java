18dc268a39b459db28922b4b02ada225487e4c8b
VidiaJauzaa <vidiashadidah24@gmail.com>
2026-06-10 19:22:06 +0800

    Add files via upload
    first commit

---
+++ 2410010473_SistemInformasiAkademik/src/pkg2410010473_sisteminformasiakademik/Main.java
@@ -0,0 +1,8 @@
+package pkg2410010473_sisteminformasiakademik;
+import form.frameUtama;
+
+public class Main {
+    public static void main(String[] args) {
+        new frameUtama().setVisible(true);
+    }
+}
