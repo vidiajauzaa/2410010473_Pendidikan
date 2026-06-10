61d79137eb15d39de15776dbf02c6ae75aa0acde
VidiaJauzaa <vidiashadidah24@gmail.com>
2026-06-10 19:33:13 +0800

    Delete 2410010473_SistemInformasiAkademik directory

--- 2410010473_SistemInformasiAkademik/src/pkg2410010473_sisteminformasiakademik/Main.java
+++
@@ -1,8 +0,0 @@
-package pkg2410010473_sisteminformasiakademik;
-import form.frameUtama;
-
-public class Main {
-    public static void main(String[] args) {
-        new frameUtama().setVisible(true);
-    }
-}
